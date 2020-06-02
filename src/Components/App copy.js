8fb0ed4f21b3eeaa417abcbc9f68db71e6b7ebe8
Santu Roy <roysantu2002@yahoo.com>
2020-06-02 16:38:21 +0530

    first commit 03

---
+++ src/Components/App copy.js
@@ -0,0 +1,110 @@
+import React from "react";
+import * as BooksAPI from "../Utils/BooksAPI";
+import "../App.css";
+import CurrentlyReading from "./Books";
+import { Route } from "react-router-dom";
+import Header from "./Header";
+import Books from "./Books";
+import SearchBook from "./SearchBook";
+// import Reading from "./Reading";
+
+class BooksApp extends React.Component {
+  state = {
+    allBooks: [],
+    currentlyReading: [
+      {
+        id: 1,
+        title: "Richard Kalehoff",
+        imageLinks: {
+          smallThumbnail:
+            "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+        },
+      },
+    ],
+    read:[],
+    //screen: 'list'
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((allBooks) => {
+      this.setState(() => ({
+        allBooks,
+      }));
+    });
+  }
+
+  currentlyReading = () => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+    ContactsAPI.remove(contact);
+  }
+ 
+  // onAdd = (contact) => {
+  //   ContactsAPI.create(contact)
+  //     .then((contact) => {
+  //       this.setState((currentState) => ({
+  //         contacts: currentState.contacts.concat([contact])
+  //       }))
+  //     })
+  // }
+
+  onSearchBook = (books) => {
+    // ContactsAPI.create(contact)
+    //   .then((contact) => {
+    //     this.setState((currentState) => ({
+    //       contacts: currentState.contacts.concat([contact])
+    //     }))
+    //   })
+  };
+
+  render() {
+    return (
+      <div className='app'>
+         {console.log(this.state.allBooks)}
+        <Header />
+        <div className='list-books-content'>
+          {/* <Reading></Reading>
+          <WantToRead /> */}
+
+          <div className='list-books-content'>
+            {/* <Reading></Reading>
+          <WantToRead /> */}
+            <Books
+              header='currently reading'
+              wantToRead={this.state.currentlyReading}
+              onAdd={{}}
+            />
+          </div>
+
+          <Books
+            header='want to read'
+            wantToRead={this.state.allBooks}
+            onAdd={{}}
+          />
+           <Books
+            header='read'
+            wantToRead={this.state.read}
+            onAdd={{}}
+          />
+        </div>
+
+        <Route
+          path='/searchbook'
+          render={({ history }) => (
+            <SearchBook
+              onSearchBook={(books) => {
+                this.onSearchBook(books);
+                history.push("/");
+              }}
+            />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default BooksApp;
--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,10 @@ import SearchBook from "./SearchBook";
 
 class BooksApp extends React.Component {
   state = {
+
     allBooks: [],
+    reading:[],
+    wantToRead:[],
     currentlyReading: [
       {
         id: 1,
@@ -22,6 +25,7 @@ class BooksApp extends React.Component {
       },
     ],
     read:[],
+    readtest:[]
     //screen: 'list'
   };
 
@@ -30,9 +34,43 @@ class BooksApp extends React.Component {
       this.setState(() => ({
         allBooks,
       }));
+
+      const reading = this.state.allBooks.filter(book => book.shelf.includes('currently'))
+      this.setState({ reading });
+
+      const read = this.state.allBooks.filter(book => book.shelf === 'read')
+      this.setState({ read });
+
+      const wantToRead = this.state.allBooks.filter(book => book.shelf.includes('want'))
+      this.setState({ wantToRead });
     });
   }
 
+  // BooksList () {
+  //   const [books, updateBooks] = React.useState([]);
+    
+ 
+  //       const read = this.state.allBooks.filter(book => book.shelf === 'read')
+  //       updateBooks([...books, { name: read}]);
+  
+  // }
+
+  // const showingContacts = query === ''
+  // ? contacts
+  // : contacts.filter((c) => (
+  //     c.shelf.toLowerCase().includes('query.toLowerCase()')
+  //   ))
+  // const currentlyReading = () => {
+  //   this.setState((currentState) => ({
+  //     read: currentState.contacts.filter((c) => (
+  //       c.readingModes.shelf.toLowerCase().includes('currentlyReading')
+  //     ))
+  //   }))
+  // }
+ 
+  // checkShelf(shelf) {
+  //   return shelf >= 18;
+  // }
   // onAdd = (contact) => {
   //   ContactsAPI.create(contact)
   //     .then((contact) => {
@@ -51,27 +89,69 @@ class BooksApp extends React.Component {
     //   })
   };
 
+  // readBook(e) {
+  //   const read = this.state.allBooks.filter(book => book.shelf === 'read')
+  //   console.log(read);
+  //   if(read) {
+  //     this.setState({
+  //       read: [ ...this.state.read, read],
+  //     }, () => {
+  //       console.log(this.state.read);
+  //     });
+  //   }
+  // }
+
+
+
+
   render() {
+
+    // const read = this.state.allBooks.filter(book => book.shelf === 'read') 
+    // this.setState({
+    //   read: [ ...this.state.read, {
+    //     read
+    //   }],
+    // });
+
+    // const showingContacts = this.state.allBooks.filter((c) => (
+    //   c.readingModes.shelf.toLowerCase().includes('currentlyReading')
+    //   ))
+
+    
+    // const setCurrentlyReading = () => {
+    //   this.setState({ read: read });
+    // };
+
+    //this.state.read = this.state.allBooks.filter(book => book.shelf === 'read') 
+
+    //this.state.read = this.state.allBooks.filter(book => book.shelf === 'read')
+
+    
+    //this.setState.readtest = readtest
+
+  
     return (
+
       <div className='app'>
+
+        console.log(BooksList)
+         {/* {console.log(this.state.allBooks)} */}
         <Header />
         <div className='list-books-content'>
-          {/* <Reading></Reading>
-          <WantToRead /> */}
+          
 
           <div className='list-books-content'>
-            {/* <Reading></Reading>
-          <WantToRead /> */}
-            <Books
-              header='currently reading'
-              wantToRead={this.state.currentlyReading}
-              onAdd={{}}
-            />
+
           </div>
+          <Books
+            header='currently reading'
+            wantToRead={this.state.reading}
+            onAdd={{}}
+          />
 
           <Books
             header='want to read'
-            wantToRead={this.state.allBooks}
+            wantToRead={this.state.wantToRead}
             onAdd={{}}
           />
            <Books
